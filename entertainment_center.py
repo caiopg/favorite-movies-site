c1635d036219b0b66bf26bd8ed25758eff932a23
Caio Gandra <caio.gandra@gmail.com>
2017-12-16 12:37:44 -0200

    Made code styling modifications and added comments and documentation to code.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,49 @@
 import fresh_tomatoes
 from media import Movie
 
-#Creation of favorite movie objects
+# Start creation of favorite movie objects
+
+# Creation of Matrix Movie object: title, poster url, trailer url
 matrix = Movie("Matrix",
-               "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # NOQA
                "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
+# Creation of Matrix Reloaded Movie object: title, poster url, trailer url
 matrix_reloaded = Movie("Matrix Reloaded",
-               "https://upload.wikimedia.org/wikipedia/en/b/ba/Poster_-_The_Matrix_Reloaded.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/b/ba/Poster_-_The_Matrix_Reloaded.jpg",  # NOQA
                "https://www.youtube.com/watch?v=HVrGMnk5E_M")
 
+# Creation of Matrix Revolutions Movie object: title, poster url, trailer url
 matrix_revolutions = Movie("Matrix Revolutions",
-               "https://upload.wikimedia.org/wikipedia/pt/9/94/Matrix_revolutions.jpg",
+               "https://upload.wikimedia.org/wikipedia/pt/9/94/Matrix_revolutions.jpg",  # NOQA
                "https://www.youtube.com/watch?v=NhoaoTZJSX4")
 
+# Creation of Inception Movie object: title, poster url, trailer url
 inception = Movie("Inception",
-               "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  # NOQA
                "https://www.youtube.com/watch?v=8hP9D6kZseM")
 
+# Creation of Shutter Island Movie object: title, poster url, trailer url
 shutter_island = Movie("Shutter Island",
-               "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg",  # NOQA
                "https://www.youtube.com/watch?v=5iaYLCiq5RM")
 
-shutter_island = Movie("Shutter Island",
-               "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg",
-               "https://www.youtube.com/watch?v=5iaYLCiq5RM")
-
+# Creation of Interstellar Movie object: title, poster url, trailer url
 interstellar = Movie("Interstellar",
-               "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",  # NOQA
                "https://www.youtube.com/watch?v=zSWdZVtXT7E")
 
-#Putting all movie objects into list
-movies = [matrix, matrix_reloaded, matrix_revolutions, inception, shutter_island, interstellar]
+# End of creation of favorite movie objects
 
-#Creation of html for website
+# Putting all movie objects into list
+movies = [
+    matrix,
+    matrix_reloaded,
+    matrix_revolutions,
+    inception,
+    shutter_island,
+    interstellar
+]
+
+# Creation of html for website
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,6 +1,22 @@
-class Movie():
+class Movie:
+    ''' This class represents a movie.
+
+    Attributes:
+        title(string): Title of movie
+        poster_image_url(string): Url of movie poster image
+        trailer_youtube_url(string): Youtube url of movie trailer
+
+    '''
 
     def __init__(self, title, poster, trailer):
+        '''Constructor method for Movie class
+
+        Inputs:
+            title(string): Title of movie
+            poster: Url of movie poster image
+            trailer: Youtube url of movie trailer
+        '''
+
         self.title = title
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
